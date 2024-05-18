bf1fb8c6261b91ddd1aa0ba6af993cdcdefa6184
Memtra <zkhrddnk@mail.ru>
2024-05-18 03:14:56 +0300

    fix my calc

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int leftDigit;
-    static int rightDigit;
-    static String op;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите пример");
         String[] array = scanner.nextLine().split(" ");
-        leftDigit = Integer.parseInt(array[0]);
-        rightDigit = Integer.parseInt(array[2]);
-        op = array[1];
-        System.out.println(calc());
+
+        System.out.println(calc(array));
     }
-    public static String calc() {
+    public static String calc(String[] array) {
+        int leftDigit = Integer.parseInt(array[0]);
+        int rightDigit = Integer.parseInt(array[2]);
+        String operation = array[1];
+        String result = operation(leftDigit, operation, rightDigit);
+        return result;
+    }
+
+    public static String operation(int leftDigit, String operation, int rightDigit){
         if (leftDigit > 10 || leftDigit <= 0) {
             throw new RuntimeException();
         }
         if (rightDigit > 10 || rightDigit <= 0) {
             throw new RuntimeException();
         }
-        if (op.equals("+")) {
+        if (operation.equals("+")) {
             return String.valueOf(leftDigit + rightDigit);
         }
-        if (op.equals("-")) {
+        if (operation.equals("-")) {
             return String.valueOf(leftDigit - rightDigit);
         }
-        if (op.equals("*")) {
-            return  String.valueOf(leftDigit * rightDigit);
+        if (operation.equals("*")) {
+            return String.valueOf(leftDigit * rightDigit);
         }
-        if (op.equals("/")) {
-            return String.valueOf( leftDigit / rightDigit);
+        if (operation.equals("/")) {
+            return String.valueOf(leftDigit / rightDigit);
         }
         throw new RuntimeException();
     }
+
 }
